3a756aaf050aba80a9be9ed792ca3f2004abda72
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2018-10-05 11:28:46 -0500

    Actualizando

---
+++ Tarea1/src/tarea1/Estudiante.java
@@ -0,0 +1,41 @@
+
+package tarea1;
+public class Estudiante {
+    public String nombre,apellido;
+    public int edad,codigo;       
+       public Estudiante(){
+    }
+
+    public String getNombre() {
+        
+        return nombre;       
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+}
