77bf650f6dd7a472f494134b4da15ad3f847fa08
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2018-10-05 11:30:01 -0500

    Test

---
+++ Tarea1/src/tarea1/TestEstudiante.java
@@ -0,0 +1,13 @@
+
+package tarea1;
+public class TestEstudiante {
+    public static void main(String[] args) {
+      Objeto est1 = new Objeto();
+      Objeto est2 = new Objeto();
+    }
+}
+class Objeto{
+    public void crear(){
+        Estudiante ojb1 = new Estudiante();
+    }
+}
